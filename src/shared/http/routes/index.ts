6c4e2346ab612d40f81ce15a47e0c637ac0bda45
Lucas Travagin Pereira <lucas.travagin.ter>
2021-06-27 09:29:50 -0300

    Criação do serviço de rotas dos usuários

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
-import productsRouter from '@modules/products/routes/products.routes'
 import { Router } from 'express'
+import productsRouter from '@modules/products/routes/products.routes'
+import usersRouter from '@modules/users/routes/users.routes'
 
 const routes = Router()
 
 routes.use('/v1/products', productsRouter)
+routes.use('/v1/users', usersRouter)
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Hello Dev' })
